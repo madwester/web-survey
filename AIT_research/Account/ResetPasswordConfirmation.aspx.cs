131c92047a6efc1254cc6038f410d037547f893d
5856 <5856@ait-sydney.net.au>
2018-05-03 11:05:16 +1000

    first commit

---
+++ AIT_research/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace AIT_research.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ AIT_research/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AIT_research.Startup))]
+namespace AIT_research
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
